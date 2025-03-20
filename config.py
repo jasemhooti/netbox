c44c8b4f35e6a6df852a40e0284963063e2847bc
جاسم حوتی <59818587+jasemhooti@users.noreply.github.com>
2025-03-20 13:38:15 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+XUI_URL = "http://your-xui.com:54321"
+XUI_API_KEY = "YOUR_API_KEY"
+
+DB_URL = "postgresql://user:password@localhost/vpn_bot"
